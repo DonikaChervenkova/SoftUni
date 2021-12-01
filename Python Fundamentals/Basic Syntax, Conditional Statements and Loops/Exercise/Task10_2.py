a29f9421bffc8fc46b4855317c22450a5f9d0088
Donika Chervenkova <donika.chervenkova@gmail.com>
2021-12-01 15:19:00 +0200

    task 10_2

---
+++ Python Fundamentals/Basic Syntax, Conditional Statements and Loops/Exercise/Task10_2.py
@@ -0,0 +1,39 @@
+quantity = int(input())
+days = int(input())
+
+price_ornament_set = 2
+price_tree_skirt = 5
+price_tree_garlands = 3
+price_tree_lights = 15
+
+total_cost = 0
+christmas_spirit = 0
+
+for current_day in range(1, days + 1):
+    if current_day % 11 == 0:
+        quantity += 2
+
+    if current_day % 10 == 0:
+        total_cost += (price_tree_skirt + price_tree_garlands + price_tree_lights)
+        christmas_spirit -= 20
+
+        if current_day == days:
+            christmas_spirit -= 30
+
+    if current_day % 5 == 0:
+        total_cost += (quantity * price_tree_lights)
+        christmas_spirit += 17
+
+    if current_day % 15 == 0:
+        christmas_spirit += 30
+
+    if current_day % 3 == 0:
+        total_cost += (quantity * (price_tree_skirt + price_tree_garlands))
+        christmas_spirit += 13
+
+    if current_day % 2 == 0:
+        total_cost += (price_ornament_set * quantity)
+        christmas_spirit += 5
+
+print(f"Total cost: {total_cost}")
+print(f"Total spirit: {christmas_spirit}")
