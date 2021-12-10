8160d597f7ce3d4a13a18d0912dc19ba1444f1fb
Donika Chervenkova <donika.chervenkova@gmail.com>
2021-12-10 20:10:55 +0200

    task 6

---
+++ Python Fundamentals/Data Types and Variables/Exercise/Task06.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+for letter_1 in range(97, 97 + n):
+    for letter_2 in range(97, 97 + n):
+        for letter_3 in range(97, 97 + n):
+            print(chr(letter_1)+chr(letter_2)+chr(letter_3))
+
+
+
