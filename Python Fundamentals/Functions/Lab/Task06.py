4b3366409336eaa27b4de8667094c7ace1917e69
Donika Chervenkova <donika.chervenkova@gmail.com>
2022-01-04 19:13:01 +0200

    task 6

---
+++ Python Fundamentals/Functions/Lab/Task06.py
@@ -0,0 +1,10 @@
+width = int(input())
+height = int(input())
+
+
+def calculate_rectangle_area(w, h):
+    rect_area = w * h
+    return rect_area
+
+
+print(calculate_rectangle_area(width, height))
