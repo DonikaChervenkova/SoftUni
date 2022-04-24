10af8d701decf9956ba310007eb4a19d9160a4b4
Donika Chervenkova <donika.chervenkova@gmail.com>
2022-04-24 10:53:49 +0300

    task 2

---
+++ Python Advanced/Error Handling/Lab/Task02.py
@@ -0,0 +1,8 @@
+class Error(Exception):
+    pass
+class ValueCannotBeNegative(Error):
+    """Raised when the input value is negative"""
+for _ in range(5):
+    num = int(input())
+    if num < 0:
+        raise ValueCannotBeNegative
